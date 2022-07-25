1a0463c8051a361eeac47e9722965c1e37490c9e
KhalilJouaneh <khalil_jouaneh@yahoo.com>
2022-07-25 12:04:45 +0300

    added modal bootstrapp, openModal & closeModal functions

--- index.js
+++ index.js
@@ -21,4 +21,17 @@ async function connect() {
     }
 }
 
+function openModal() {
+    document.getElementById("token_modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("token_modal").style.display = "none";
+}
+
+
+//onClick events
+document.getElementById("from_token_select").onclick = openModal;
+document.getElementById("modal_close").onclick = closeModal;
+
 document.getElementById("login_button").onclick = connect;
